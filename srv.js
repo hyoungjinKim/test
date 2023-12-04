1b47adf0760dac930b0f5f8f1a62a5eed9880f8d
HYJIN\isacc <isacc7224@gmail.com>
2023-12-04 20:32:04 +0900

    no message

--- srv.js
+++ srv.js
@@ -2,8 +2,11 @@ const express=require('express')
 const mysql= require('mysql2')
 const path = require('path')//public directory에 있는 파일을 사용하기 위해 사용
 const static = require('serve-static')//srv.js가 가장 최상위 경로로 인식 하기 위해 사용
-
+const cookieParser =require('cookie-parser')
+const expressSession=require('express-session')
+const bodyParser =require('body-parser')
 const dbconfig = require(`./config/dbconfig.json`)//db정보 받아옴
+const serveStatic = require('serve-static')
 
 
 //Database connection pool
@@ -18,10 +21,19 @@ const pool = mysql.createPool({
 })
 
 const app = express()
-app.use(express.urlencoded({extended:true}))//URL을 인코딩 확장할 수 있음
+app.use(serveStatic(path.join(__dirname,`public`)));
+app.use(bodyParser.urlencoded({extended:false}))//URL을 인코딩 확장할 수 있음
 app.use(express.json())//json형태로 오는 것 확인 가능
+app.use(cookieParser());
 
-app.use('/process/adduser', static(path.join(__dirname,`public`)));
+app.use(
+    expressSession({
+      secret: "my key",
+      resave: true,
+      saveUninitialized: true,
+    })
+);
+
 //현재 디렉토리에/public 추가
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'adduser.html'));
@@ -29,6 +41,34 @@ app.get('/', (req, res) => {
 
 app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'login.html'));
+});
+
+app.get("/process/example", (req, res) => {
+    if (req.session.user) {
+      // 세션에 유저가 존재한다면
+      res.redirect("/example.html"); // 예시로
+    } else {
+      res.redirect("/login"); // fhrmdlsdmfh
+    }
+});
+
+app.get("/process/logout", (req, res) => {
+    console.log("로그아웃");
+  
+    if (req.session.user) {
+      console.log("로그아웃중입니다!");
+      req.session.destroy((err) => {
+        if (err) {
+          console.log("세션 삭제시에 에러가 발생했습니다.");
+          return;
+        }
+        console.log("세션이 삭제됐습니다.");
+        res.redirect("/login");
+      });
+    } else {
+      console.log("로그인이 안돼있으시네요?");
+      res.redirect("/login");
+    }
 });
 
 app.post('/process/adduser', async (req, res) => {
@@ -66,8 +106,7 @@ app.post('/process/adduser', async (req, res) => {
 app.post('/process/login', (req,res)=>{
     console.log('/process/login req'+req);
     const paramId=req.body.id;
-    const paramPassword=req.body.password;
-
+    const paramPassword = req.body.password;
     console.log(`로그인 요청${paramId,paramPassword}`);
 
     pool.getConnection((err,con)=>{
@@ -81,7 +120,7 @@ app.post('/process/login', (req,res)=>{
             return;
         }
 
-        const exec = con.query("select `id`, `username` from `users` where `id`=? and `password`= SHA2(?,256)",
+        const exec = con.query("select `id`, `password` from `users` where `id`=? and `password`= SHA2(?,256);",
             [paramId,paramPassword],
             (err,rows)=>{
                 con.release();
@@ -98,8 +137,26 @@ app.post('/process/login', (req,res)=>{
 
                 if(rows.length>0){
                     console.log(`아이디[${paramId}]찾음`);
-                    res.writeHead('200',{'Content-Type':'text/html; charset=utf8'})
-                    res.write('<h2>login success</h2>')
+                    if (req.session.user) {
+                        // 세션에 유저가 존재한다면
+                        console.log("이미 로그인 돼있습니다~");
+                        res.writeHead(200, { "Content-Type": "text/html; charset=utf8" });
+                        res.write("<h1> already Login</h1>");
+                        res.write(`[ID] : ${paramId} [PW] : ${paramPassword}`);
+                        res.write('<a href="/process/example">예시로</a>');
+                        res.end();
+                      } else {
+                        req.session.user = {
+                          id: paramId,
+                          pw: paramPassword,
+                          authorized: true,
+                        };
+                        res.writeHead(200, { "Content-Type": "text/html; charset=utf8" });
+                        res.write("<h1>Login Success</h1>");
+                        res.write(`[ID] : ${paramId} [PW] : ${paramPassword}`);
+                        res.write('<a href="/process/example">Move</a>');
+                        res.end();
+                      };                   
                     res.end();
                     return;
                 }
